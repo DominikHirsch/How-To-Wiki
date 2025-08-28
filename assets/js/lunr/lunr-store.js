2d3aaedd38e99355e5fd8d11fe0f2394e0f85178
DominikHirsch <DominikHirsch@users.noreply.github.com>
2025-08-28 15:22:30 +0000

    deploy: 93348d845e563eb9b74b69b23a783dbfbad7f211

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -2,24 +2,24 @@ var store = [{
         "title": "Seite 1",
         "excerpt":"Seite 1   Das ist Seite 1 im Wiki.  ","categories": [],
         "tags": [],
-        "url": "/seite1/",
+        "url": "/How-To-Wiki/seite1/",
         "teaser": null
       },{
         "title": "Seite 2",
         "excerpt":"Seite 2   Dies ist Seite 2 im Wiki.  Sie hat zwei Unterseiten.  ","categories": [],
         "tags": [],
-        "url": "/seite2/",
+        "url": "/How-To-Wiki/seite2/",
         "teaser": null
       },{
         "title": "Unterseite 1",
         "excerpt":"Unterseite 1   Das ist die erste Unterseite.  ","categories": [],
         "tags": [],
-        "url": "/seite2/sub1/",
+        "url": "/How-To-Wiki/seite2/sub1/",
         "teaser": null
       },{
         "title": "Unterseite 2",
         "excerpt":"Unterseite 2   Das ist die zweite Unterseite.  ","categories": [],
         "tags": [],
-        "url": "/seite2/sub2/",
+        "url": "/How-To-Wiki/seite2/sub2/",
         "teaser": null
       },]
